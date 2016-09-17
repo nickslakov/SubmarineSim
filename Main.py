c9c17821748dc00ae9126a607f4de50fc0e1d626
GramG <ggg_1192@hotmail.com>
2016-09-17 12:26:19 +0200

    Did some work in main... Yay

--- Main.py
+++ Main.py
@@ -1,3 +1,27 @@
-import vector
-import sub
+import vector as vec
+import sub as uBoat
+import numpy as np
+import Tkinter as tk
 
+magnitude = 2.0  #Max value of fluid flow (m/s)
+deltaT = 0.01    #Timestep
+timer = 0
+height = 250 #Height and width of window
+width = 300
+
+#Vector components are computed using a uniform random variable
+waterFlow = vec.Vector(np.random.uniform(-magnitude,magnitude),np.random.uniform(-magnitude,magnitude))
+Sean = uBoat.Sub() #Initialize the submarine
+
+#Initialize the simulation window
+window = tk.Tk()
+c = tk.Canvas(window, bg = "grey", height = height, width = width )
+c.pack()
+
+def updatedisplay(sub, waterFlow, window):
+    c.create_polygon(25, 50, -25, 50, -25, -50, 25, -50, fill="orange")
+    sub.update(deltaT, waterFlow)
+    window.after(10, updatedisplay(sub, waterFlow))
+
+window.after(0, updatedisplay(Sean, waterFlow))
+window.mainloop()

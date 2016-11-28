61e494dbb0d06bd489b0d47e4aaba78821f56c05
Nick <Nick>
2016-11-28 15:52:30 +0100

    Final RNN and Main Push.

--- Main.py
+++ Main.py
@@ -1,67 +1,43 @@
-import vector as vec
 import sub as uBoat
 import RNN
 import numpy as np
-import Tkinter as tk
-import time
-import random as rng
 import matplotlib.pyplot as plt
 
 deltaT = 0.01    #Timestep (s)
-timer = 0
-height = 750 #Height and width of window
-width = 1000
 NUM_SUBS = 10000
 SEQUENCE_LENGTH = 35
+SCALE_FACTOR = 10
 
 subs = []
 
 for i in range(NUM_SUBS):
     subs.append(uBoat.Sub())
 
-
-#f = open('Output.txt', 'w')
-#f.write(str(waterFlow.x) + ' ' + str(waterFlow.y))
-#f.close()
-
-#Initialize the simulation window
-#window = tk.Tk()
-#c = tk.Canvas(window, bg = "grey", height = height, width = width )
-#c.pack()
-
-#shape = c.create_polygon(25, 50, -25, 50, -25, -50, 25, -50, fill="orange")
-
 data = np.zeros((NUM_SUBS,SEQUENCE_LENGTH,6))
 
-def writeFile(vals):
-    f = open('Output.txt', 'a')
-    for val in vals:
-        f.write(str(val.x) + ' ' + str(val.y) + ' ')
-    f.write('\n')
-    f.close()
+#Generate data for all subs.
 
 i = 0
 for sub in subs:
     for j in range(SEQUENCE_LENGTH):
         current_positionVec, current_thrustVec, current_waterFlow = sub.update(deltaT)
-        data_timestep = [10*current_positionVec.x, 10*current_positionVec.y,current_thrustVec.x, current_thrustVec.y, current_waterFlow.x, current_waterFlow.y]
+        data_timestep = [SCALE_FACTOR*current_positionVec.x, SCALE_FACTOR*current_positionVec.y,current_thrustVec.x, current_thrustVec.y, current_waterFlow.x, current_waterFlow.y]
 
-        #writeFile(data[i])
         data[i,j,:] = data_timestep
     i += 1
 
-
+#Plot some example sequences
 fig, ax = plt.subplots()
 ax.plot(data[0,:,0], data[0,:,1], '.b-')
 ax.plot(data[1,:,0], data[1,:,1], '.r-')
 ax.plot(data[2,:,0], data[2,:,1], '.g-')
 plt.show()
 
-#print "Calling rnn"
+#Call RNN
 RNN.rnn(data)
 
 
-#window.mainloop()
+
 
 
 
--- RNN.py
+++ RNN.py
@@ -1,10 +1,10 @@
 import lasagne
 import theano
 import theano.tensor as T
-import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-#from IPython import display
+import collections
+import csv
 
 
 def rnn(data):
@@ -16,49 +16,60 @@ def rnn(data):
 
     #Start of Network
 
-    NUM_BATCHES = 1000
-    BATCH_SIZE = 500
+    NUM_BATCHES = 3000
+    BATCH_SIZE = 1000
     SEQUENCE_LENGTH = 34
     NUM_INPUTS = 6
     NUM_OUTPUTS = 2
-    CLIP_VAL = 5
+    CLIP_VAL = 2
 
     x_sym = T.tensor3() # Symbolic inputs
     y_sym = T.tensor3() # Symbolic target outputs
 
-    l_in = lasagne.layers.InputLayer(shape = (BATCH_SIZE, None, NUM_INPUTS))
-    l_hid1 = lasagne.layers.GRULayer(l_in, num_units=30, learn_init=True)
-    #l_hid2 = lasagne.layers.GRULayer(l_hid1, num_units=10, learn_init=True)
-    #l_hid3 = lasagne.layers.GRULayer(l_hid2,num_units=10,learn_init=True)
+
+    init_state = theano.shared(np.zeros((BATCH_SIZE,30)))
+    l_in_secondary = lasagne.layers.InputLayer(shape = (None, 30))
+
+    l_in = lasagne.layers.InputLayer(shape = (None, None, NUM_INPUTS))
+    l_hid1 = lasagne.layers.GRULayer(l_in, num_units=30, hid_init=l_in_secondary)
     l_reshape = lasagne.layers.ReshapeLayer(l_hid1,(-1,30))
     l_dense = lasagne.layers.DenseLayer(l_reshape, num_units=NUM_OUTPUTS, nonlinearity=None)
-    l_out = lasagne.layers.ReshapeLayer(l_dense, (BATCH_SIZE, None, NUM_OUTPUTS))
+    l_out = lasagne.layers.ReshapeLayer(l_dense, (BATCH_SIZE, SEQUENCE_LENGTH, NUM_OUTPUTS))
+
+
+    l_out_eval = lasagne.layers.ReshapeLayer(l_dense, (BATCH_SIZE,1,NUM_OUTPUTS))
 
     # Define the training and evaluation outputs.
-    train_out = lasagne.layers.get_output(l_out, {l_in: x_sym}, deterministic = False)
-    eval_out = lasagne.layers.get_output(l_out, {l_in: x_sym}, deterministic = True)
+    train_out = lasagne.layers.get_output(l_out, {l_in: x_sym, l_in_secondary: init_state}, deterministic = False)
+    hidden_state, eval_out = lasagne.layers.get_output([l_hid1, l_out_eval], {l_in: x_sym, l_in_secondary: init_state})
+
+    # Updates for evaluation
+    hidden_update = collections.OrderedDict()
+    hidden_update[init_state] = hidden_state[:,-1,:]
+
+    # Reset updates for after evaluation
+    reset_update = collections.OrderedDict()
+    reset_update[init_state] = np.zeros((BATCH_SIZE,30))
 
     # All trainable params in the network.
     all_params = lasagne.layers.get_all_params(l_out, trainable=True)
 
     # Define the Cost Function.
     cost_train = lasagne.objectives.squared_error(train_out, y_sym).mean()
-    cost_eval = lasagne.objectives.squared_error(eval_out, y_sym).mean()
 
     # Use Theano to compute all of the gradients. Included gradient clipping to avoid gradient explosion.
-    #all_grads = T.grad(cost_train, all_params)
     all_grads = [T.clip(g,-CLIP_VAL,CLIP_VAL) for g in T.grad(cost_train, all_params)]
-    #all_grads = lasagne.updates.total_norm_constraint(all_grads,CLIP_VAL) #Could modify the clipping values if needed.
 
     # Compile the update gradient.
     updates = lasagne.updates.adam(all_grads, all_params, learning_rate=0.007)
 
     # Compile the Theano functions.
     train_func = theano.function(inputs = [x_sym, y_sym], outputs = [cost_train, train_out], updates=updates)
-    eval_func = theano.function(inputs = [x_sym, y_sym], outputs = [cost_eval, eval_out])
+    eval_func = theano.function(inputs=[x_sym], outputs = [eval_out], updates=hidden_update)
+    reset_func = theano.function(inputs = [x_sym], outputs = [hidden_state], updates=reset_update)
 
     #Start of the training loop.
-    train_cost, val_cost = [],[]
+    train_cost, eval_cost = [], []
 
     for e in range(NUM_BATCHES):
 
@@ -73,60 +84,91 @@ def rnn(data):
         x_current = train_data_current[:,:-1,:]
         y_current = train_data_current[:,1:,:2]
 
-        out = train_func(x_current,y_current)
-        denom1 = []
+        denom = []
+        train_out = train_func(x_current,y_current)
         for i in range(BATCH_SIZE):
             for j in range(SEQUENCE_LENGTH):
-                denom1.append((x_current[i,j,0]-y_current[i,j,0])**2 + (x_current[i,j,1]-y_current[i,j,1])**2)
+                denom += [(x_current[i,j,0]-y_current[i,j,0])**2 + (x_current[i,j,1]-y_current[i,j,1])**2]
 
-        train_cost += [np.sqrt(out[0]/np.mean(denom1))]
+        train_cost += [np.sqrt(train_out[0]/np.mean(denom))]
 
+        # Start Eval
 
-        # Start evaluation
-
-        index_eval = np.random.randint(2000,size=BATCH_SIZE)
-        eval_data_current = np.zeros((BATCH_SIZE,SEQUENCE_LENGTH + 1,NUM_INPUTS))
+        index_eval = np.random.randint(2000, size=BATCH_SIZE)
+        eval_data_current = np.zeros((BATCH_SIZE, SEQUENCE_LENGTH + 1, NUM_INPUTS))
         for i in range(BATCH_SIZE):
-            eval_data_current[i,:,:] = data_eval[index_eval[i],:,:]
+            eval_data_current[i, :, :] = data_eval[index_eval[i], :, :]
+        x_current2 = eval_data_current[:, :-1, :]
+        y_current2 = eval_data_current[:, 1:, :2]
 
-        x_current = eval_data_current[:,:-1,:]
-        y_current = eval_data_current[:,1:,:2]
+        x_test = np.zeros((BATCH_SIZE, 1, NUM_INPUTS))
+        x_test[:, :, :] = x_current2[:, :1, :]
 
-        out = eval_func(x_current,y_current)
+        eval_out = np.zeros((BATCH_SIZE, SEQUENCE_LENGTH, 2))
+        for i in range(SEQUENCE_LENGTH):
+            x_test[:, :, 2:] = x_current2[:, i:i + 1, 2:]
+            eval_out[:, i:i + 1, :] = eval_func(x_test)[0]
+            x_test[:, :, :2] = eval_out[:, i:i + 1, :]
 
         denom2 = []
         for i in range(BATCH_SIZE):
-            for j in range(SEQUENCE_LENGTH):
-                denom2.append((x_current[i,j,0]-y_current[i,j,0])**2 + (x_current[i,j,1]-y_current[i,j,1])**2)
+            denom2 += [y_current2[i, -1, 0] ** 2 + y_current2[i, -1, 1] ** 2]
 
-        val_cost += [np.sqrt(out[0]/np.mean(denom2))]
+        eval_cost += [np.sqrt(lasagne.objectives.squared_error(eval_out[:,-1,:],y_current2[:,-1,:]).mean()/np.mean(denom2))]
+
+        reset_func(x_current2)
 
         # Printouts
 
         if (e + 1) % 100 == 0:
             print 'Batch number: ' + repr(e)
             print '\tMean of last ten training costs: ' + repr(np.mean(train_cost[-10:-1]))
-            print '\tMean of last ten evaluation costs: ' + repr(np.mean(val_cost[-10:-1]))
+            print '\tMean of last ten evaluation costs: ' + repr(np.mean(eval_cost[-10:-1]))
             print '\tMin training cost since last printout: ' + repr(np.min(train_cost[-99:-1]))
             print '\tMax training cost since last printout: ' + repr(np.max(train_cost[-99:-1]))
 
         # Plot some examples at termination
 
-        if e == NUM_BATCHES-1 or train_cost[e] < 0.04:
-            fig, ax = plt.subplots()
-            ax.plot(y_current[0,:,0], y_current[0,:,1], '.r-')
-            ax.plot(out[1][0,:,0], out[1][0,:,1], 'xb-')
+        if e == NUM_BATCHES-1 or train_cost[e] < 0.05:
+            x_test = np.zeros((BATCH_SIZE,1,6))
+            x_test[:,:,:] = x_current[:,:1,:]
 
-            ax.plot(y_current[1, :, 0], y_current[1, :, 1], '.g-')
-            ax.plot(out[1][1, :, 0], out[1][1, :, 1], 'xk-')
+            eval_out = np.zeros((BATCH_SIZE,SEQUENCE_LENGTH,2))
+            for i in range(SEQUENCE_LENGTH):
+                x_test[:, :, 2:] = x_current[:, i:i + 1, 2:]
 
-            plt.show()
+                eval_out[:,i:i+1,:] = eval_func(x_test)[0]
+
+                x_test[:,:,:2] = eval_out[:,i:i+1,:]
+
+            # Some example plots
+            for i in range(10):
+                fig, ax = plt.subplots()
+                ax.plot(y_current[i, :, 0], y_current[i, :, 1], '.k-')
+                ax.plot(eval_out[i, :, 0], eval_out[i, :, 1], 'xb-')
+                ax.plot(train_out[1][i, :, 0], train_out[1][i, :, 1], '.r-')
+                plt.show()
+
+                fig, ax = plt.subplots()
+                ax.plot(y_current[i, :, 0], y_current[i, :, 1], '.k-')
+                ax.plot(train_out[1][i, :, 0], train_out[1][i, :, 1], 'xr-')
+                plt.show()
+
+                fig, ax = plt.subplots()
+                ax.plot(y_current[i,:,0], y_current[i,:,1], '.k-')
+                plt.show()
+
             break
 
+    # Plot cost. Remove early costs to see structure in cost in later epochs
     fig, ax = plt.subplots()
-    ax.plot(range(len(train_cost) - 100), train_cost[100:], '.k-')
-    ax.plot(range(len(val_cost) - 100), val_cost[100:], '.b-')
+    ax.plot(range(100,len(train_cost)), train_cost[100:], '.k-')
+    ax.plot(range(100,len(eval_cost)), eval_cost[100:], 'xb-')
     plt.show()
+
+    with open('cost.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile, deliminator=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(train_cost)
 
 
 
